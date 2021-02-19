323057b2b24a2df0cbb3825643371fdac8c08f78
kazu <2020kazutaka@gmail.com>
2021-02-19 15:08:15 +0900

    [Add] Vue.jsを導入
    ESLint、prettierも追加

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,12 +1,12 @@
+/* eslint-disable no-undef */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require('@rails/ujs').start();
+require('@rails/activestorage').start();
+require('channels');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
