84828d807fb59b60dab91d11f315eb77cfbfe7bc
kazu <2020kazutaka@gmail.com>
2021-02-28 23:26:23 +0900

    [Style] Lintエラーの修正

--- app/controllers/concerns/api/arrangement_base64.rb
+++ app/controllers/concerns/api/arrangement_base64.rb
@@ -1,37 +1,39 @@
-module Api::ArrangementBase64
-  extend ActiveSupport::Concern
+module Api
+  module ArrangementBase64
+    extend ActiveSupport::Concern
 
-  def create_uploadedfile(encode_data, filename = "arrangement_image")
-    image_data = split_uri(encode_data)
-    decode_data = Base64.decode64(image_data[:form_data])
-    image_params = {
-      filename: "#{filename}.#{image_data[:extension]}",
-      type: image_data[:content_type],
-      tempfile: create_tempfile(decode_data),
-    }
-    ActionDispatch::Http::UploadedFile.new(image_params)
-  end
+    def create_uploadedfile(encode_data, filename = 'arrangement_image')
+      image_data = split_uri(encode_data)
+      decode_data = Base64.decode64(image_data[:form_data])
+      image_params = {
+        filename: "#{filename}.#{image_data[:extension]}",
+        type: image_data[:content_type],
+        tempfile: create_tempfile(decode_data)
+      }
+      ActionDispatch::Http::UploadedFile.new(image_params)
+    end
 
-  private
+    private
 
-  def split_uri(data)
-    # decodeするにはdata:*/*;base64,を取り除く
-    data.match(%r{^data:(.+);.+,(.+)$})
-    uri = Hash.new
-    uri[:content_type] = $1
-    uri[:form_data] = $2
-    uri[:extension] = $1.split('/')[1]
-    uri
-  end
+    def split_uri(data)
+      # decodeするにはdata:*/*;base64,を取り除く
+      data =~ /^data:(.+);.+,(.+)$/
+      uri = {}
+      uri[:content_type] = Regexp.last_match(1)
+      uri[:form_data] = Regexp.last_match(2)
+      uri[:extension] = Regexp.last_match(1).split('/')[1]
+      uri
+    end
 
-  #https://docs.ruby-lang.org/ja/2.3.0/class/Tempfile.html
-  # 画像などのバイナリデータを書き込む時は、binmodeを使用する。
-  def create_tempfile(data)
-    Tempfile.open('imagefile') do |file|
-      file.binmode
-      file.write(data)
-      file.rewind
-      file
+    # https://docs.ruby-lang.org/ja/2.3.0/class/Tempfile.html
+    # 画像などのバイナリデータを書き込む時は、binmodeを使用する。
+    def create_tempfile(data)
+      Tempfile.open('imagefile') do |file|
+        file.binmode
+        file.write(data)
+        file.rewind
+        file
+      end
     end
   end
 end
