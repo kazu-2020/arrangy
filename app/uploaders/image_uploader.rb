5d959bceb0f74ffb2b2d116c0567eb635743e346
kazu <2020kazutaka@gmail.com>
2021-03-01 19:20:37 +0900

    [Fix] ImageUploaderを修正
    vue側と統一するため

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,16 +1,18 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+  # アップロードされたファイルの保存先(storage :fileの場合)
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # 画像サイズのバリデーション(https://github.com/carrierwaveuploader/carrierwave/wiki/How-to%3A-Validate-image-file-size)
+  def size_range
+    0.byte..10.megabytes
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -21,27 +23,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
+  # vue側と統一
+  process resize_to_fit: [300, 300]
+  process convert: 'png'
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
 
-  # Add an allowlist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
+  # 拡張子のホワイトリスト
   def extension_allowlist
     %w[jpg jpeg png gif]
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 end
