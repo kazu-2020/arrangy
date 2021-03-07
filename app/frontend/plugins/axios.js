532eb9bd1239eefd6686ef796e489e6cf2de071c
kazu <2020kazutaka@gmail.com>
2021-03-07 09:09:23 +0900

    [Fix] headからcsrf-tokenを取得する方法を修正
    既存の方法だとsystem-specでエラーが発生してしまう為

--- app/frontend/plugins/axios.js
+++ app/frontend/plugins/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { csrfToken } from 'rails-ujs';
 
-let csrf_token = document.getElementsByName('csrf-token')[0].content;
+// let csrf_token = document.getElementsByName('csrf-token')[0].content;
 const axiosInstance = axios.create({
   baseURL: '/api',
-  headers: { 'X-CSRF-TOKEN': csrf_token },
+  headers: { 'X-CSRF-TOKEN': csrfToken() },
 });
 
 export default axiosInstance;
