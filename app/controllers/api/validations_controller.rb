1de95116bcf9c9c9708cac00538be8605041e1df
kazu <2020kazutaka@gmail.com>
2021-03-15 16:01:42 +0900

    [Fix] ユーザー新規登録時に既存の値を使用できるバグを修正

--- app/controllers/api/validations_controller.rb
+++ app/controllers/api/validations_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class ValidationsController < ApplicationController
     def unique
-      result = User.where('NOT(id = ?) and (nickname = ? or email= ?)', params[:id], params[:nickname], params[:email]).exists? ? 'exist' : 'unique'
+      result = User.where.not(id: params[:id]).where('nickname = ? or email= ?', params[:nickname], params[:email]).exists? ? 'exist' : 'unique'
       render json: { meta: result }
     end
   end
