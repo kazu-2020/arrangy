759ea0d00683e170e80941d177c9d4f2fce50c30
kazu <2020kazutaka@gmail.com>
2021-03-16 20:28:19 +0900

    [Add] ヘッダーにメニューリストを追加

--- app/frontend/components/global/TheHeader.vue
+++ app/frontend/components/global/TheHeader.vue
@@ -1,33 +1,32 @@
 <template>
   <div>
-    <v-app-bar fixed height="60" elevation="1" color="grey lighten-5">
+    <v-app-bar id="page-header" fixed height="60" elevation="1" class="px-10" color="#FAFAFA">
       <v-toolbar-title>
-        <router-link class="router-link text-h4" style="color: #d32f2f" :to="{ name: 'TopPage' }">
+        <router-link class="router-link text-h4" style="color: #ff5252" :to="{ name: 'TopPage' }">
           ARRANGY
         </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <template v-if="!!authUser">
-        <v-btn text :to="{ name: 'UserProfile' }">マイページ</v-btn>
-        <v-btn text :to="{ name: 'ArrangementNew' }">新規投稿</v-btn>
-        <v-btn text>お気に入り一覧</v-btn>
-        <v-btn text rounded plain :ripple="{ center: true }" xLarge @click="logoutFunction">
-          ログアウト
-        </v-btn>
+      <template v-if="authUser">
+        <v-btn plain text xLarge :to="{ name: 'ArrangementNew' }">新規投稿</v-btn>
+        <v-menu bottom rounded offsetY>
+          <template #activator="{ on }">
+            <v-btn icon xLarge v-on="on">
+              <v-avatar>
+                <img :src="authUser.avatar" />
+              </v-avatar>
+            </v-btn>
+          </template>
+          <v-list dense flat>
+            <v-list-item text plain :to="{ name: 'UserProfile' }">マイページ</v-list-item>
+            <v-list-item text>お気に入り一覧</v-list-item>
+            <v-list-item text @click="logoutFunction"> ログアウト </v-list-item>
+          </v-list>
+        </v-menu>
       </template>
       <template v-else>
-        <v-btn :to="{ name: 'UserRegister' }" text rounded plain :ripple="{ center: true }" xLarge>
-          新規登録
-        </v-btn>
-        <v-btn
-          class="hidden-sm-and-down"
-          :to="{ name: 'UserLogin' }"
-          text
-          rounded
-          plain
-          :ripple="{ center: true }"
-          xLarge
-        >
+        <v-btn plain text xLarge :to="{ name: 'UserRegister' }"> 新規登録 </v-btn>
+        <v-btn class="hidden-sm-and-down" plain text xLarge :to="{ name: 'UserLogin' }">
           ログイン
         </v-btn>
       </template>
