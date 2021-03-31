eb9bbd2fa43a91215b46a0cf344d0c9419158013
kazu <2020kazutaka@gmail.com>
2021-03-31 14:51:46 +0900

    [Add] 自身の投稿一覧についてのテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,15 @@ FactoryBot.define do
     sequence(:email) { |n| "example#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
+
+    trait :with_arrangements do
+      transient do
+        count { 5 }
+      end
+
+      after(:build) do |user, evaluator|
+        user.arrangements << create_list(:arrangement, evaluator.count)
+      end
+    end
   end
 end
