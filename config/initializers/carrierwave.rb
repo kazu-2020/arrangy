96ea6bf1a1b8a983e53681f3af08b0ea47050320
kazu <2020kazutaka@gmail.com>
2021-04-08 14:44:56 +0900

    [Fix] fogの設定を本番環境でのみ読み込む

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,14 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
     config.enable_processing = true
+
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: Rails.application.credentials.aws[:region]
+    }
+    config.fog_directory  = 'arrangy'
   elsif Rails.env.development?
     config.storage = :file
     config.enable_processing = true
@@ -9,13 +17,4 @@ CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
   end
-
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: Rails.application.credentials.aws[:region]
-  }
-
-  config.fog_directory  = 'arrangy'
 end
