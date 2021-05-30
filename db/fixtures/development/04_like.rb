dacda46795adc9f07a154fa548ce34bab4300213
kazu <2020kazutaka@gmail.com>
2021-05-30 14:32:34 +0900

    [Update] 開発環境用のseedデータを更新

--- db/fixtures/development/04_like.rb
+++
@@ -1,8 +0,0 @@
-User.ids.count.times do |n|
-  Like.seed(
-    :id,
-    { id: n, arrangement_id: Arrangement.first.id, user_id: n }
-  )
-end
-
-Arrangement.reset_counters(Arrangement.first.id, :likes)
---
+++ db/fixtures/development/06_like.rb
@@ -0,0 +1,12 @@
+User.ids.each do |n|
+  Like.seed(
+    :id,
+    {
+      id: n,
+      arrangement_id: Arrangement.first.id,
+      user_id: n
+    }
+  )
+end
+
+Arrangement.reset_counters(Arrangement.first.id, :likes)
